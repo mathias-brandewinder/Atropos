b7ce18af83fc2f46c3a4f5fdf45f4787fc105371
Mathias Brandewinder <mathias.brandewinder@gmail.com>
2016-06-17 11:44:38 +0200

    Refined SVM configuration and corresponding Titanic example.

--- Atropos/Atropos.Examples/SVM.fsx
+++ Atropos/Atropos.Examples/SVM.fsx
@@ -1,4 +1,5 @@
 ﻿#I @"../../packages/"
+#r @"Accord.Statistics/lib/net45/Accord.Statistics.dll"
 #r @"../Atropos/Atropos/bin/Debug/Atropos.dll"
 #r @"../Atropos/Atropos.SVM/bin/Debug/Atropos.SVM.dll"
 
@@ -6,6 +7,7 @@ open Atropos.Core
 open Atropos.Utilities
 open Atropos.Metrics
 open Atropos.SVM
+open Accord.Statistics.Kernels
 
 #r @"FSharp.Data/lib/net40/FSharp.Data.dll"
 open FSharp.Data
@@ -22,28 +24,42 @@ let ``classification model`` () =
 
     let ``passenger age`` : FeatureLearner<Passenger,bool> =
         fun sample ->
-            let avg = avgReplace (sample |> Seq.map (fun (p,_) -> p.Age))
-            fun pass -> pass.Age |> avg |> Continuous
+            fun pass -> pass.Age |> Continuous
 
     let ``passenger class`` : FeatureLearner<Passenger,bool> =
         fun sample ->
             fun pass -> Discrete ([|"1";"2";"3"|], pass.Pclass |> string) 
 
-    let gender : FeatureLearner<Passenger,bool> =
+    let ``passenger gender`` : FeatureLearner<Passenger,bool> =
         fun sample ->
             fun pass -> Discrete ([|"male";"female"|], pass.Sex)
 
     let model = [
         ``passenger age``
         ``passenger class``
-        gender
+        ``passenger gender``
         ]
+
+    let config = { 
+        SVM.DefaultConfig with 
+            Tolerance = 1e-3
+            Kernel = Gaussian ()
+            Complexity = Some 5.0 }
 
     let svmClassifier = 
         survivalSample
-        |> SVM.classification SVM.DefaultConfig model 
+        |> SVM.classification config model 
 
     survivalSample
+    // probably an issue here: what is predicted
+    // when there is an issue with featurization,
+    // i.e. the vector contains NaNs?
+    // TODO figure out how to handle.
     |> Seq.map (fun (o,l) -> 
         svmClassifier o, l)
+    |> Seq.map (fun (p,l) -> 
+        printfn "Pred: %b, Act: %b" p l
+        p,l)
     |> accuracy
+
+``classification model`` ()
